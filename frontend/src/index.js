d4d394a67c01c8d64582876362e31cb0ac133589
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-05-22 20:35:45 +0530

    React-router-dom installed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/styles/bootstrap.min.css";
 import "./assets/styles/index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
