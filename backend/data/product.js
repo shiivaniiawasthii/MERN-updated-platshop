0d2eb95b88514620ce147758a1f0a169a7c5ab18
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-05-23 18:58:40 +0530

    added data to mongo with users

--- backend/data/product.js
+++ backend/data/product.js
@@ -1,7 +1,5 @@
 const products = [
   {
-    _id: "1",
-
     name: "Pothos",
     image:
       "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pothos-indoor-plant-1662666398.png?crop=0.719xw:0.894xh;0.105xw,0.106xh&resize=980:*",
@@ -15,8 +13,33 @@ const products = [
     countInStock: 0,
   },
   {
-    _id: "2",
+    name: "ZZ plant",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/concept-of-home-gardening-zamioculcas-in-flowerpot-royalty-free-image-1580854121.jpg?crop=0.833xw:0.833xh;0.107xw,0.167xh&resize=980:*",
+    description:
+      "ZZ plants require less water than a lot of plants. If you tend to neglect yours (rather than overwater them), you'll do well with this one. It'll thrive in bright, indirect light—but it can also tolerate low light if you have a less-than-sunny spot you want to keep it in, according to Bloomscap",
 
+    brand: "Care level: Easy",
+    category: "Indoor",
+    rating: 4,
+
+    price: 300,
+    countInStock: 8,
+  },
+  {
+    name: "Pothos",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/concept-o…",
+    description:
+      "Succulents only require a small amount of water, so this set of four succulents is sure to spruce up your home without requiring much maintenance",
+    brand: "Care level: Medium",
+    category: "Indoor",
+    rating: 9,
+
+    price: 400,
+    countInStock: 10,
+  },
+  {
     name: "Succulents",
     image:
       "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pothos-indoor-plant-1662666398.png?crop=0.719xw:0.894xh;0.105xw,0.106xh&resize=980:*",
