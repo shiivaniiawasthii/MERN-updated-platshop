5724a1db49997322b1013e5bbe84715855453f17
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-05-23 20:42:07 +0530

    code-cleanup and errorhandlers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,13 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404).json({ message: "Product not found" });
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
